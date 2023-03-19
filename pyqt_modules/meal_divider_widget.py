f2e853603eba83dff57f80955dbc9c41cf0ccda3
Jon Edvin Lona <jone.lona@gmail.com>
2023-03-19 23:37:09 +0100

    Added part_size labels that scale seemlessly

--- pyqt_modules/meal_divider_widget.py
+++ pyqt_modules/meal_divider_widget.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QPushButton, QApplication, QWidget, QHBoxLayout
+from PyQt6.QtWidgets import QPushButton, QApplication, QWidget, QHBoxLayout, QLineEdit, QLabel
 from PyQt6.QtCore import Qt, QPoint
 from PyQt6.QtGui import QMouseEvent
 
@@ -16,19 +16,63 @@ class MealDividerWidget(QWidget):
         self.background.setFixedHeight(30)
         self.background.setEnabled(False)
 
+        # Lists to hold dividers and part sizes
         self.dividers = []
+        self.part_sizes = []
+
+        # Add one part size
+        self.addPartSize()
 
         # Build
         self.general_layout = QHBoxLayout()
-        self.general_layout.setContentsMargins(0,10,0,10)
+        self.general_layout.setContentsMargins(0,10,0,50)
         self.general_layout.addWidget(self.background)
         self.setLayout(self.general_layout)
+
 
     def addDivider(self):
         """ Adds a divider. """
         new_divider = Divider(self)
         new_divider.move(self.mapToGlobal(self.pos()))
         self.dividers.append(new_divider)
+        self.addPartSize()
+
+
+    def addPartSize(self):
+        """ Adds a part_size. """
+        self.part_sizes.append(QLabel(parent=self))
+        self.part_sizes[-1].setFixedWidth(40)
+        self.updatePartSizes()
+
+
+    def updatePartSizes(self):
+        """ Updates the part sizes so that everything matches. """
+        y = self.pos().y() + self.background.height()
+        start_x = self.pos().x()
+        end_x = self.pos().x() + self.background.width()
+        length = end_x - start_x
+
+        left_x = start_x
+        for i, divider in enumerate(self.dividers):
+            # Get x's
+            part_x = divider.pos().x()
+            middle_x = left_x + (part_x-left_x)/2
+
+            # Update part_size
+            part_size = self.part_sizes[i]
+            new_pos = QPoint(int(middle_x-part_size.width()/2), int(y))
+            part_size.move(new_pos)
+            part_size.setText(str(round((part_x - left_x) / length, 2)))
+            
+            left_x = part_x
+
+        # Last one
+        middle_x = left_x + (end_x-left_x)/2
+        part_size = self.part_sizes[-1]
+        new_pos = QPoint(int(middle_x), int(y))
+        part_size.move(new_pos)
+        part_size.setText(str(round((end_x - left_x) / length, 2)))
+
 
     def getParts(self) -> list:
         """ Returns a list of parts, adding up to 1, """
@@ -37,7 +81,7 @@ class MealDividerWidget(QWidget):
 
         # Get bar parameters
         start_x = self.mapToGlobal(self.pos()).x()
-        end_x = self.mapToGlobal(self.pos()).x() + self.width()
+        end_x = self.mapToGlobal(self.pos()).x() + self.background.width()
         length = end_x - start_x
 
         # Add parts
@@ -46,7 +90,7 @@ class MealDividerWidget(QWidget):
             part_x = self.mapToGlobal(divider.pos()).x()
             parts.append((part_x - left_x) / length)
             left_x = part_x
-        parts.append((end_x - left_x) / length)
+        parts.append(round((end_x - left_x) / length, 2))
 
         for part in parts:
             print(part)
@@ -64,12 +108,14 @@ class Divider(QPushButton):
 
         self.__mouseMovePos = None
 
+
     def mousePressEvent(self, event: QMouseEvent):
         """ Overridden to save press location. """
         if event.button() == Qt.MouseButton.LeftButton:
             self.__mouseMovePos = event.globalPosition()
 
         return super().mousePressEvent(event)
+
 
     def mouseMoveEvent(self, event: QMouseEvent):
         """ Overriden to move widget with mouse if left mouse button is held down. """
@@ -84,7 +130,16 @@ class Divider(QPushButton):
             self.move(newPos)
             self.__mouseMovePos = mousePos
 
+            # Update part_sizes
+            self.parent().updatePartSizes()
+
         super().mouseMoveEvent(event)
+
+    
+class PartSize(QLineEdit):
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+        self.setFixedWidth(40)
 
 
 if __name__ == "__main__":
